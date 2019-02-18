092e14f42516d6f5ac25244c0d230d95090ca13e
William Thio <williamthio886@gmail.com>
2019-02-18 17:05:23 +0700

    Add compile_run.cpp

---
+++ compile_run.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <unistd.h>
+#include <limits.h>
+#include <cstring>
+#include <cstdio>
+#include <sys/stat.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    const char *kDirPath = "~/Desktop/cp3";
+
+    if (argc == 1) {
+        cout << "File name required.\n";
+        return 0;
+    }
+
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+
+    char filename[PATH_MAX];
+    sprintf(filename, "%s/%s", cwd, argv[1]);
+
+    struct stat fbuffer;
+    if (stat(filename, &fbuffer) != 0) {
+        cout << "File not exist.\n";
+        return 0;
+    }
+
+    char *dot = strrchr(filename, '.');
+    if (!dot) {
+        cout << "Invalid file type.\n";
+        return 0;
+    }
+
+    char cmd[PATH_MAX + 27];
+
+    if (!strcmp(dot, ".cpp")) {
+        sprintf(cmd, "g++ -o %s/bin \"%s\"", kDirPath, filename);
+        system(cmd);
+        sprintf(cmd, "%s/bin < %s/input.txt > %s/output.txt", kDirPath, kDirPath, kDirPath);
+        system(cmd);
+        sprintf(cmd, "rm %s/bin", kDirPath);
+        system(cmd);
+    }
+    else if (!strcmp(dot, ".java")) {
+        sprintf(cmd, "javac -d %s \"%s\"", kDirPath, filename);
+        system(cmd);
+        sprintf(cmd, "java -cp %s Main < %s/input.txt > %s/output.txt", kDirPath, kDirPath, kDirPath);
+        system(cmd);
+        sprintf(cmd, "rm %s/Main.class", kDirPath);
+        system(cmd);
+    }
+    else {
+        cout << "Unrecognized file type.\n";
+    }
+
+    sprintf(cmd, "cp \"%s\" %s/code.txt", filename, kDirPath);
+    system(cmd);
+
+    return 0;
+}
+
