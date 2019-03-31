95cb7244a6b72be55ff7cd75401099a77d11f119
William Thio <williamthio886@gmail.com>
2019-03-31 15:16:29 +0700

    Update compile_run.cpp

--- compile_run.cpp
+++ compile_run.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    const char *kDirPath = "~/outputs";
+    const char *kDirPath = "~/io";
 
     if (argc == 1) {
         cout << "File name required.\n";
@@ -36,28 +36,28 @@ int main(int argc, char** argv) {
     char cmd[PATH_MAX * 3 + 50];
 
     if (!strcmp(dot, ".cpp")) {
-        sprintf(cmd, "g++ -o \"%s\"/bin \"%s\"", kDirPath, filename);
+        sprintf(cmd, "g++ -o %s/bin \"%s\"", kDirPath, filename);
         system(cmd);
-        sprintf(cmd, "\"%s\"/bin < \"%s\"/input.txt > \"%s\"/output.txt", kDirPath, kDirPath, kDirPath);
+        sprintf(cmd, "%s/bin < %s/input.txt > %s/output.txt", kDirPath, kDirPath, kDirPath);
         system(cmd);
-        sprintf(cmd, "rm \"%s\"/bin", kDirPath);
+        sprintf(cmd, "rm %s/bin", kDirPath);
         system(cmd);
     }
     else if (!strcmp(dot, ".java")) {
-        sprintf(cmd, "javac -d \"%s\" \"%s\"", kDirPath, filename);
+        sprintf(cmd, "javac -d %s \"%s\"", kDirPath, filename);
         system(cmd);
-        sprintf(cmd, "java -cp \"%s\" Main < \"%s\"/input.txt > \"%s\"/output.txt", kDirPath, kDirPath, kDirPath);
+        sprintf(cmd, "java -cp %s Main < %s/input.txt > %s/output.txt", kDirPath, kDirPath, kDirPath);
         system(cmd);
-        sprintf(cmd, "rm \"%s\"/Main.class", kDirPath);
+        sprintf(cmd, "rm %s/Main.class", kDirPath);
         system(cmd);
     }
     else {
         cout << "Unrecognized file type.\n";
     }
 
-    sprintf(cmd, "cp \"%s\" \"%s\"/code.txt", filename, kDirPath);
+    sprintf(cmd, "cp \"%s\" %s/code.txt", filename, kDirPath);
     system(cmd);
-    sprintf(cmd, "cat \"%s\"/output.txt", kDirPath);
+    sprintf(cmd, "cat %s/output.txt", kDirPath);
     system(cmd);
 
     return 0;
